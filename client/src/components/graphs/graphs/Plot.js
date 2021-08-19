3fa574fafc66b313445fa17bbc9b30fe3e122edb
ecuber <37519236+ecuber@users.noreply.github.com>
2021-08-19 02:44:16 -0700

    Lint

--- client/src/components/graphs/graphs/Plot.js
+++ client/src/components/graphs/graphs/Plot.js
@@ -1,4 +1,4 @@
-import React, { Suspense, lazy } from 'react'
+import React from 'react'
 import { data, layout, config } from '../../../content/plotly.json'
 import Plotly from 'react-plotly.js'
 import { Box } from '@chakra-ui/layout'
